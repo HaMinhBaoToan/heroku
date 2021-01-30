6af68a75653db53ffa4f094b8d1cbab937f91ad3
hmbt93@gmail.com <hmbt93@gmail.com>
2021-01-30 12:44:32 +0700

    adadad

--- backend/server/utils/db.js
+++ backend/server/utils/db.js
@@ -1,10 +1,11 @@
 const knex = require('knex')({
-  client: 'mysql',
+  client: 'mysql2',
   connection: {
-    // host: 'us-cdbr-east-03.cleardb.com',
-    // user: 'b0fcb0931e803b',
-    // password: 'ffaab3f2',
-    database: 'mysql://b0fcb0931e803b:ffaab3f2@us-cdbr-east-03.cleardb.com/heroku_7fac5bf006f4d9a?reconnect=true',
+    host: 'us-cdbr-east-03.cleardb.com',
+    user: 'b0fcb0931e803b',
+    password: 'ffaab3f2',
+    database: 'heroku_7fac5bf006f4d9a',
+    // database: 'mysql://b0fcb0931e803b:ffaab3f2@us-cdbr-east-03.cleardb.com/heroku_7fac5bf006f4d9a?reconnect=true',
   },
   // pool: {
   //   min: 0,
