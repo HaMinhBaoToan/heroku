efcc97314141d1e4e2664b8952bdce006705848c
hmbt93@gmail.com <hmbt93@gmail.com>
2021-01-30 11:46:32 +0700

    no message

--- backend/server/utils/db.js
+++ backend/server/utils/db.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
   client: 'mysql2',
   connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '123456',
-    database: 'qlkd',
-    port: 3366
+    host: 'us-cdbr-east-03.cleardb.com',
+    user: 'b0fcb0931e803b',
+    password: 'ffaab3f2',
+    database: 'heroku_7fac5bf006f4d9a',
+    port: 3306
   },
   pool: {
     min: 0,
