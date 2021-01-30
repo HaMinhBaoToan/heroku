acd23fafa01d55be196b93674f613fb9e0fb2dd5
hmbt93@gmail.com <hmbt93@gmail.com>
2021-01-30 12:28:32 +0700

    no message

--- backend/server/utils/db.js
+++ backend/server/utils/db.js
@@ -1,15 +1,15 @@
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: 'us-cdbr-east-03.cleardb.com',
-    user: 'b0fcb0931e803b',
-    password: 'ffaab3f2',
-    database: 'heroku_7fac5bf006f4d9a',
+    // host: 'us-cdbr-east-03.cleardb.com',
+    // user: 'b0fcb0931e803b',
+    // password: 'ffaab3f2',
+    database: 'mysql://b0fcb0931e803b:ffaab3f2@us-cdbr-east-03.cleardb.com/heroku_7fac5bf006f4d9a?reconnect=true',
   },
-  pool: {
-    min: 0,
-    max: 50
-  }
+  // pool: {
+  //   min: 0,
+  //   max: 50
+  // }
 });
 
 module.exports = knex;
